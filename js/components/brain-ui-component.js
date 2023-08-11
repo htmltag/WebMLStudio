95ee6a8ef8d4de98b7f8c0d9aeeb2eccc668d4d1
Jonathan <jonathan.soyland.lier@adera.no>
2023-08-11 17:06:56 +0200

    Brain Training

--- js/components/brain-ui-component.js
+++ js/components/brain-ui-component.js
@@ -46,12 +46,19 @@ text-align: center;
     width: 25%;
     height: 100%;
 }
+
+#network-render {
+    background-color: #fff;
+    min-height: 400px;
+}
 </style>
 <div>
     <nav>
         <ul>
             <li><a href="#" role="button" id="load-btn">Load</a></li>
             <li><a href="#" role="button" id="network-btn">Network</a></li>
+            <li><a href="#" role="button" id="training-btn">Training</a></li>
+            <li><a href="#" role="button" id="testing-btn">Testing</a></li>
             <li><a href="#" role="button" id="empty-btn" class="warning"><i class="fa-solid fa-trash-can"></i></a></li>
         </ul>
     </nav>
@@ -121,19 +128,55 @@ text-align: center;
             </label>
             <label for="network-leaky-relu-alpha">
                 Leaky Relu Alpha
-                <input type="text" id="network-binarythresh" placeholder="Leaky Relu Alpha">
+                <input type="text" id="network-leaky-relu-alpha" placeholder="Leaky Relu Alpha">
             </label>
         </div>
+        <button id="network-render-graph">Render</button>
     </div>
-    
+    <div id="panel-training">
+        <div class="grid">
+            <label for="training-iterations">
+                Iterations
+                <input type="text" id="training-iterations" placeholder="Max Iterations">
+            </label>
+            <label for="training-error-thresh">
+                Error Thresh
+                <input type="text" id="training-error-thresh" placeholder="Error percentage">
+            </label>
+            <label for="training-learning-rate">
+                Training rate
+                <input type="text" id="training-learning-rate" placeholder="Learning rate">
+            </label>
+        </div>
+        <button id="start-training-btn">Start training</button>
+    </div>
+    <div id="panel-testing">
+        <div class="grid">
+            <input type="text" id="testing-columns" placeholder="Array of input columns"/>
+            <button id="testing-generate-form-btn">Generate form</button>
+        </div>
+        <div id="testing-form-wrapper"></div>
+        <div id="testing-result-wrapper">
+            <button id="testing-run-btn">Run</button>
+            <span id="testing-result"></span> 
+        </div>
+    </div>
+
     <div id="danfo-output">
         <div id="danfo-inputdata"></div>
         <div id="danfo-outputdata"></div>
+    </div>
+    <div id="network-render"></div>
+    <div id="training-output">
+        <div id="training-output-log"></div>
+        <progress id="training-progress" value="0" max="100"></progress>
     </div>
 </div>
 `;
 
 class BrainUIEditor {
+    #shadow;
+
     #dfInput;
     #dfOutput;
     #datastores;
@@ -144,9 +187,17 @@ class BrainUIEditor {
     #brainTrainingData;
 
     #selectedNetwork;
+    #selectedNetworkActivation;
+    #networkConfig;
+    #network;
+
+    #iterations;
+    #errorThresh;
+    #trainingProgress;
     
 
     constructor(shadow) {
+        this.#shadow = shadow;
         // Load Danfo data
         this.DANFO_OUTPUT_ELEMENT = shadow.querySelector("#danfo-output");
         this.DANFO_OUTPUTDATA = shadow.querySelector("#danfo-outputdata");
@@ -168,9 +219,33 @@ class BrainUIEditor {
         // Network
         this.SELECTED_NETWORK = shadow.querySelector("#select-network");
         this.NETWORK_SIZE = shadow.querySelector("#network-size");
+        this.NETWORK_BINARYTHRESH = shadow.querySelector("#network-binarythresh");
+        this.SELECTED_NETWORK_ACTIVATION = shadow.querySelector("#select-activation");
+        this.NETWORK_LEAKY_RELU_ALPHA = shadow.querySelector("#network-leaky-relu-alpha");
+        this.NETWORK_RENDER = shadow.querySelector("#network-render");
 
         shadow.querySelector("#select-network").addEventListener('change', this.selectedNetworkChange.bind(this));
-        
+        shadow.querySelector("#select-activation").addEventListener('change', this.selectedNetworkChange.bind(this));
+        shadow.querySelector("#network-render-graph").addEventListener('click', this.renderNetworkGraph.bind(this));
+
+        //Training
+        this.TRAINING_ITERATIONS = shadow.querySelector("#training-iterations");
+        this.TRAINING_ERROR_THRESH = shadow.querySelector("#training-error-thresh");
+        this.TRAINING_LEARNING_RATE = shadow.querySelector("#training-learning-rate");
+        this.TRAINING_OUTPUT_LOG = shadow.querySelector("#training-output-log");
+        this.TRAINING_PROGRESS = shadow.querySelector("#training-progress");
+
+        shadow.querySelector("#training-btn").addEventListener('click', this.initTraining.bind(this));
+        shadow.querySelector("#start-training-btn").addEventListener('click', this.startTraining.bind(this));
+
+        //testing
+        this.TESTING_INPUT_COLUMNS = shadow.querySelector("#testing-columns");
+        this.TESTING_FORM = shadow.querySelector("#testing-form-wrapper");
+        this.TESTING_RESULT = shadow.querySelector("#testing-result");
+
+        shadow.querySelector("#testing-btn").addEventListener('click', this.initTesting.bind(this));
+        shadow.querySelector("#testing-generate-form-btn").addEventListener('click', this.generateTestingForm.bind(this));
+        shadow.querySelector("#testing-run-btn").addEventListener('click', this.runTest.bind(this));
         
         this.init();
     }
@@ -373,8 +448,15 @@ class BrainUIEditor {
         }
     }
 
+    selectedNetworkActivationChange(){
+        this.#selectedNetworkActivation = this.SELECTED_NETWORK_ACTIVATION.options[this.SELECTED_NETWORK_ACTIVATION.selectedIndex].value;
+    }
+
+    //Initialize neural network with default values
     initNeuralNetwork() {
         this.calculateNetworkSize();
+        this.NETWORK_BINARYTHRESH.value = 0.5;
+
     }
 
     initRecurrentNeuralNetwork() {
@@ -402,13 +484,191 @@ class BrainUIEditor {
         this.NETWORK_SIZE.value = s;
     }
 
+    renderNetworkGraph() {
+        switch (this.#selectedNetwork) {
+            case "NEURALNETWORK":
+                this.renderNeuralNetwork();
+                break;
+            case "RECURRENTNEURALNETWORK":
+                renderRecurrentNeuralNetwork();
+                break;
+        }
+    }
+
+    renderNeuralNetwork() {
+        this.generateNeuralNetworkConfig();
+        this.#network = new brain.NeuralNetwork(this.#networkConfig);
+        this.NETWORK_RENDER.innerHTML = brain.utilities.toSVG(this.#network, {inputs:{labels: this.#dfInput.columns}});
+    }
+
+    renderRecurrentNeuralNetwork() {}
+
+    generateNeuralNetworkConfig() {
+        if (this.NETWORK_SIZE.value === "") this.calculateNetworkSize();
+        const size = this.NETWORK_SIZE.value.split(',').map((item) => {return parseInt(item, 10)});
+        this.#networkConfig = {
+            inputSize: size[0],
+            inputRange: size[0],
+            hiddenLayers: size.slice(1, size.length - 1),
+            outputSize: size[size.length - 1],
+            leakyReluAlpha: this.NETWORK_LEAKY_RELU_ALPHA.value === "" ? 0.01 : this.NETWORK_LEAKY_RELU_ALPHA.value,
+            binaryThresh: this.NETWORK_BINARYTHRESH.value === "" ? 0.5 : this.NETWORK_BINARYTHRESH.value,
+            activation: this.#selectedNetworkActivation === undefined ? 'sigmoid' : this.#selectedNetworkActivation
+        };
+    }
+
     //************** */
     // Training
     //************** */
 
+    //Initialize the training data
+    initTraining() {
+        if (this.#selectedNetwork == undefined || this.#selectedNetwork === "") toastr.error("No network type is selected");
+        switch (this.#selectedNetwork) {
+            case "NEURALNETWORK":
+                this.initTrainingNeuralnetwork();
+                break;
+            case "RECURRENTNEURALNETWORK":
+                this.initTrainingRecurrentNeuralnetwork();
+                break;
+        }
+    }
+
+    // Set default values in training UI for neural network
+    initTrainingNeuralnetwork() {
+        this.TRAINING_ITERATIONS.value = 2000;
+        this.TRAINING_ERROR_THRESH.value = 0.005;
+        this.TRAINING_LEARNING_RATE.value = 0.3;
+    }
+
+    // Set default values in training UI for recurrent neural network
+    initTrainingRecurrentNeuralnetwork() {
+        this.TRAINING_ITERATIONS.value = 2000;
+        this.TRAINING_ERROR_THRESH.value = 0.005;
+        this.TRAINING_LEARNING_RATE.value = 0.1;
+    }
+
+    startTraining() {
+        switch (this.#selectedNetwork) {
+            case "NEURALNETWORK":
+                this.trainNeuralNetwork();
+                break;
+            case "RECURRENTNEURALNETWORK":
+                this.trainReccurentNeuralNetwork();
+                break;
+        }
+    }
+
+    trainNeuralNetwork() {
+        this.#iterations = Number(this.TRAINING_ITERATIONS.value === "" ? 2000 : this.TRAINING_ITERATIONS.value);
+        this.#errorThresh = Number(this.TRAINING_ERROR_THRESH.value === "" ? 0.005 : this.TRAINING_ERROR_THRESH.value);
+        const learningRate = Number(this.TRAINING_LEARNING_RATE.value === "" ? 0.3 : this.TRAINING_LEARNING_RATE.value);
+        this.TRAINING_PROGRESS.max = this.#iterations;
+        const result = this.#network.train(this.#brainTrainingData, {
+            iterations: this.#iterations,
+            errorThresh: this.#errorThresh,
+            learningRate: learningRate,
+            log: details => this.trainingLog(details),
+            logPeriod: 10, 
+        });
+        //console.log(result);
+    }
+
+    trainReccurentNeuralNetwork() {
+        this.#iterations = Number(this.TRAINING_ITERATIONS.value === "" ? 2000 : this.TRAINING_ITERATIONS.value);
+        this.#errorThresh = Number(this.TRAINING_ERROR_THRESH.value === "" ? 0.005 : this.TRAINING_ERROR_THRESH.value);
+        const learningRate = Number(this.TRAINING_LEARNING_RATE.value === "" ? 0.3 : this.TRAINING_LEARNING_RATE.value);
+        this.TRAINING_PROGRESS.max = this.#iterations;
+        const result = this.#network.train(this.#brainTrainingData, {
+            iterations: this.#iterations,
+            errorThresh: this.#errorThresh,
+            learningRate: learningRate,
+            log: details => this.trainingLog(details),
+            logPeriod: 10,
+        });
+        //console.log(result);
+    }
+
+    trainingLog(details) {
+        this.TRAINING_PROGRESS.value = details.iterations;
+        console.log(details);
+        let output = "";
+        if(this.#errorThresh >= details.error.toFixed(2)) {
+            output = "<div style='text-align: center; padding-top:1rem; padding-bottom: 1rem; color:red;'>";
+        } else {
+            output = "<div style='text-align: center; padding-top:1rem; padding-bottom: 1rem;'>";
+        }
+
+        if(this.#iterations === details.iterations) {
+            output = "<div style='text-align: center; padding-top:1rem; padding-bottom: 1rem; color:green;'>";
+        }
+
+        output += "Error: " + (details.error * 100).toFixed(2) + "%. Limit: " + this.#errorThresh * 100 + "%";
+        output += "<br/>";
+        output += "Iterations: " + details.iterations + " of " + this.#iterations; 
+        output += "</div>";
+
+        this.TRAINING_OUTPUT_LOG.innerHTML = output;
+    }
+
     //************** */
     // Test
     //************** */
+
+
+    initTesting() {
+        if (this.#dfInput.columns.length > 0) {
+            this.TESTING_INPUT_COLUMNS.value = this.#dfInput.columns;
+        }
+    }
+    
+    generateTestingForm() {
+        if (this.TESTING_INPUT_COLUMNS.value === "") toastr.error("Missing values to generate testing form");
+
+        const inputTestColumns = this.TESTING_INPUT_COLUMNS.value.split(',');
+
+        let elements = "";
+        for (const key in inputTestColumns) {
+            if (Object.hasOwnProperty.call(inputTestColumns, key)) {
+                const element = inputTestColumns[key];
+                elements += this.getTestInputElement(element);
+            }
+        }
+
+        this.TESTING_FORM.innerHTML = elements;
+    }
+
+    getTestInputElement(name) {
+        let el = "<label for='testing-input-" + name + "'>" + name;
+        el += "<input type='text' id='testing-input-" + name + "' placeholder='input value'></label>"
+        return el;
+    }
+
+    runTest() {
+        this.TESTING_RESULT.value = "";
+
+        const testInput = this.generateTestInput;
+        console.log("test input: " + testInput);
+
+        this.TESTING_RESULT.value = this.#network.run(testInput);
+    }
+
+    generateTestInput() {
+        if (this.TESTING_INPUT_COLUMNS.value === "") toastr.error("Missing values to generate testing form");
+
+        const inputTestColumns = this.TESTING_INPUT_COLUMNS.value.split(',');
+
+        let values = {};
+        for (const key in inputTestColumns) {
+            if (Object.hasOwnProperty.call(inputTestColumns, key)) {
+                const element = inputTestColumns[key];
+                let el = this.#shadow.querySelector("#"+element);
+                values[element] = el.value;
+            }
+        }
+
+        return values;
+    }
 
 }
 
@@ -419,12 +679,21 @@ class BrainUIContentSwitcher {
         this.DANFO_OUTPUTDATA = shadow.querySelector("#danfo-outputdata");
         this.DANFO_INPUTDATA = shadow.querySelector("#danfo-inputdata");
 
+        this.NETWORK_RENDER = shadow.querySelector("#network-render");
+
+        this.TRAINING_OUTPUT = shadow.querySelector("#training-output");
+
         this.PANEL_LOAD = shadow.querySelector("#panel-load");
         this.PANEL_NETWORK = shadow.querySelector("#panel-network");
-        
+        this.PANEL_TRAINING = shadow.querySelector("#panel-training");
+        this.PANEL_TESTING = shadow.querySelector("#panel-testing");
 
+    
         shadow.querySelector("#load-btn").addEventListener('click', this.showLoad.bind(this));
         shadow.querySelector("#network-btn").addEventListener('click', this.showNetwork.bind(this));
+        shadow.querySelector("#training-btn").addEventListener('click', this.showTraining.bind(this));
+        shadow.querySelector("#testing-btn").addEventListener('click', this.showTesting.bind(this));
+        
         this.hideAll();
         this.showLoad();
     }
@@ -441,6 +710,18 @@ class BrainUIContentSwitcher {
     showNetwork() {
         this.hideAll();
         this.PANEL_NETWORK.classList.add("show");
+        this.NETWORK_RENDER.classList.add("show");
+    }
+
+    showTraining() {
+        this.hideAll();
+        this.PANEL_TRAINING.classList.add("show");
+        this.TRAINING_OUTPUT.classList.add("show");
+    }
+
+    showTesting() {
+        this.hideAll();
+        this.PANEL_TESTING.classList.add("show");
     }
 
     hideAll(){
@@ -451,6 +732,9 @@ class BrainUIContentSwitcher {
         this.DANFO_OUTPUT_ELEMENT.classList.remove("flex-grid");
         this.DANFO_OUTPUT_ELEMENT.classList.add("hide");
 
+        this.NETWORK_RENDER.classList.remove("show");
+        this.NETWORK_RENDER.classList.add("hide");
+
         this.DANFO_OUTPUTDATA.classList.remove("show");
         this.DANFO_OUTPUTDATA.classList.add("hide");
 
@@ -459,6 +743,15 @@ class BrainUIContentSwitcher {
 
         this.PANEL_NETWORK.classList.remove("show");
         this.PANEL_NETWORK.classList.add("hide");
+
+        this.PANEL_TRAINING.classList.remove("show");
+        this.PANEL_TRAINING.classList.add("hide");
+
+        this.TRAINING_OUTPUT.classList.remove("show");
+        this.TRAINING_OUTPUT.classList.add("hide");
+
+        this.PANEL_TESTING.classList.remove("show");
+        this.PANEL_TESTING.classList.add("hide");
     }
 
 }
